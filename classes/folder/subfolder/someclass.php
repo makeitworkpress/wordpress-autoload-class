df11a532468ed81dd94ef57d9e48aacb8eb82fc2
Michiel Tramper <michielsite@hotmail.com>
2019-08-08 15:26:05 +0200

    Create someclass.php

---
+++ classes/folder/subfolder/someclass.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This class is namespaced according to the folder structure!
+namespace Folder\Subfolder;
+
+class Someclass {
+    
+    private $property;
+
+    public function __construct() {
+         $this->property = 1;       
+    }
+
+    public function getProperty() {
+          return $this->property;
+    }
+
+}
