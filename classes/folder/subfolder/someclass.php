db2683a4bd8ed907063ded246cc3452e55c27b2d
Michiel Tramper <michielsite@hotmail.com>
2019-08-08 15:38:29 +0200

    Update someclass.php

--- classes/folder/subfolder/someclass.php
+++ classes/folder/subfolder/someclass.php
@@ -1,14 +1,15 @@
 <?php
 /**
  * This class is namespaced according to the folder structure!
-namespace Folder\Subfolder;
+ */
+namespace Folder;
 
 class Someclass {
     
     private $property;
 
     public function __construct() {
-         $this->property = 1;       
+         $this->property = 'Subfolder';       
     }
 
     public function getProperty() {
