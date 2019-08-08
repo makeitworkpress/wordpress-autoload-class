ab317f87681b94aace27db7e395a5771a6fa6664
Michiel Tramper <michielsite@hotmail.com>
2019-08-08 15:41:36 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,25 +1,41 @@
 <?php
-/**
- * Registers the autoloader at root level (functions.php) of a theme. It looks for PHP classes inside the classes folder.
- */
  spl_autoload_register( function($classname) {
 
-    $class = str_replace( '\\', DIRECTORY_SEPARATOR, strtolower($classname) ); 
-    $file  = dirname(__FILE__) .  DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $class . '.php';
+    // Regular
+    $class      = str_replace( '\\', DIRECTORY_SEPARATOR, strtolower($classname) ); 
+    $classpath  = dirname(__FILE__) .  DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $class . '.php';
+
+    // WordPress
+    $parts      = explode('\\', $classname);
+    $class      = 'class-' . strtolower( array_pop($parts) );
+    $folders    = strtolower( implode(DIRECTORY_SEPARATOR, $parts) );
+    $wppath     = dirname(__FILE__) .  DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $folders . DIRECTORY_SEPARATOR . $class . '.php';
     
-    if ( file_exists( $file ) ) {
-        include_once $file;
+    if ( file_exists( $classpath ) ) {
+        include_once $classpath;
+    } elseif(  file_exists( $wppath ) ) {
+        include_once $wppath;
     }
    
 } );
+
 
 /**
  * Calls an autoloaded example class
  */
 $class = new Folder\Someclass();   // Constructs the class
-echo $class->getProperty(); // Echoes 1
+echo $class->getProperty(); // Echoes Folder
+
 
 /*
  * Calls an autoloaded class, but a folder deeper
  */
-$class = new Folder\Subfolder\Someclass();
+$class = new Folder\Subfolder\Someclass();   // Constructs the class
+echo $class->getProperty(); // Echoes Folder
+
+
+/*
+ * Calls an autoloaded class according to the wordpress standard
+ */
+$class = new WordPress\Someclass();   // Constructs the class
+echo $class->getProperty(); // Echoes WordPress
